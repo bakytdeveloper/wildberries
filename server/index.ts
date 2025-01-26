772dc947de2dc730e7da287880d07d67ff81c139
bakytdeveloper <bakytdeveloper@gmail.com>
2025-01-26 17:07:04 +0600

    Обновил функционал отображение данных, при получении по запросу

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,24 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
-import { fetchAndParseProducts } from './services/productService';
 import mongoose from 'mongoose';
-import { QueryModel } from './models/queryModel';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { QueryModel } from './models/queryModel';
+import { fetchAndParseProducts } from './services/productService';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5500;
 
-mongoose.connect(process.env.MONGODB_URI!)
+mongoose.connect(process.env.MONGODB_URI!, {
+    serverSelectionTimeoutMS: 5000, // 5 seconds timeout
+})
     .then(() => {
-        console.log('Подключено к MongoDB');
+        console.log('Connected to MongoDB');
     })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error.message);
+    .catch((error: any) => {
+        console.error('Error connecting to MongoDB:', error.message, error);
     });
 
 app.use(cors({ origin: '*' }));
@@ -27,15 +29,15 @@ app.get('/api/queries', async (req: Request, res: Response) => {
         const queries = await QueryModel.find().sort({ createdAt: -1 });
         console.log('queries:', queries);
         res.json(queries);
-    } catch (error) {
-        console.error('Error fetching queries:', Error);
+    } catch (error: any) {
+        console.error('Error fetching queries:', error.message);
         res.status(500).json({ error: 'Failed to fetch queries' });
     }
 });
 
 app.get('/api/products', async (req: Request, res: Response) => {
     const { query, dest } = req.query;
-    console.log(`Полученный запрос: ${query}, dest: ${dest}`);
+    console.log(`Received query: ${query}, dest: ${dest}`);
 
     if (!query || !dest) {
         res.status(400).json({ error: 'Query and dest parameters are required' });
@@ -45,16 +47,21 @@ app.get('/api/products', async (req: Request, res: Response) => {
     try {
         const products = await fetchAndParseProducts(query as string, dest as string);
 
+        const now = new Date();
+        now.setHours(now.getUTCHours() + 3); // Московское время (UTC+3)
+        const queryTime = now.toISOString();
+
         const newQuery = new QueryModel({
             query: query,
             dest: dest,
             response: products,
+            queryTime: queryTime
         });
         await newQuery.save();
 
         res.json(products);
-    } catch (error) {
-        console.error('Error fetching products:', Error);
+    } catch (error: any) {
+        console.error('Error fetching products:', error.message);
         res.status(500).json({ error: 'Failed to fetch products' });
     }
 });
@@ -63,5 +70,10 @@ app.get('/api/products', async (req: Request, res: Response) => {
 app.use(express.static(path.join(__dirname, '../../public')));
 
 app.listen(port, () => {
-    console.log(`Сервер запущен на: http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+// Обработка маршрутов, которые не были найдены
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!");
 });
