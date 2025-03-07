fb7b27c740c70627dae35cfb776d9999b75ca05d
bakytdeveloper <bakytdeveloper@gmail.com>
2025-03-08 01:07:39 +0600

    Сделал формы регистрации и логина не активными, во время загрузки

--- client/src/components/Auth/LoginForm.js
+++ client/src/components/Auth/LoginForm.js
@@ -8,16 +8,26 @@ const LoginForm = ({ API_HOST, setIsAuthenticated, setShowProfile, setShowForgot
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
+    const [loading, setLoading] = useState(false); // Добавлено состояние loading
 
     const handleLogin = async (e) => {
         e.preventDefault();
+        setLoading(true); // Блокируем форму
         try {
             const response = await axios.post(`${API_HOST}/api/auth/login`, { email, password });
             sessionStorage.setItem('token', response.data.token);
             setIsAuthenticated(true);
             setShowProfile(true);
         } catch (error) {
-            Toastify({ text: 'Ошибка авторизации', duration: 3000, gravity: 'top', position: 'right', style: { background: '#ff0000' } }).showToast();
+            Toastify({
+                text: 'Ошибка авторизации',
+                duration: 3000,
+                gravity: 'top',
+                position: 'right',
+                style: { background: '#ff0000' },
+            }).showToast();
+        } finally {
+            setLoading(false); // Разблокируем форму
         }
     };
 
@@ -26,20 +36,51 @@ const LoginForm = ({ API_HOST, setIsAuthenticated, setShowProfile, setShowForgot
             <h2>Авторизация</h2>
             <Form.Group controlId="email">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                <Form.Control
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    disabled={loading} // Делаем поле неактивным
+                />
             </Form.Group>
             <Form.Group controlId="password">
                 <Form.Label>Пароль</Form.Label>
                 <InputGroup>
-                    <Form.Control type={showPassword ? 'text' : 'password'} value={password} onChange={(e) => setPassword(e.target.value)} required />
-                    <InputGroup.Text className="FaEye-button" onClick={() => setShowPassword(!showPassword)} >
+                    <Form.Control
+                        type={showPassword ? 'text' : 'password'}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        disabled={loading} // Делаем поле неактивным
+                    />
+                    <InputGroup.Text
+                        className="FaEye-button"
+                        onClick={() => setShowPassword(!showPassword)}
+                        style={{ cursor: 'pointer' }}
+                    >
                         {showPassword ? <FaEyeSlash /> : <FaEye />}
                     </InputGroup.Text>
                 </InputGroup>
             </Form.Group>
-            <Button type="submit">Войти</Button>
-            <Button variant="link" onClick={() => setShowForgotPasswordForm(true)}>Забыли пароль?</Button>
-            <Button variant="link" className="registration-button" onClick={() => setShowRegisterForm(true)}>Регистрация</Button>
+            <Button type="submit" disabled={loading}>
+                {loading ? 'Загрузка...' : 'Войти'}
+            </Button>
+            <Button
+                variant="link"
+                onClick={() => setShowForgotPasswordForm(true)}
+                disabled={loading} // Блокируем кнопку
+            >
+                Забыли пароль?
+            </Button>
+            <Button
+                variant="link"
+                className="registration-button"
+                onClick={() => setShowRegisterForm(true)}
+                disabled={loading} // Блокируем кнопку
+            >
+                Регистрация
+            </Button>
         </Form>
     );
 };
--- client/src/components/Auth/RegisterForm.js
+++ client/src/components/Auth/RegisterForm.js
@@ -4,24 +4,39 @@ import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import Toastify from 'toastify-js';
 import axios from 'axios';
 
-const RegisterForm = ({API_HOST, setIsAuthenticated, setShowProfile, setShowRegisterForm }) => {
+const RegisterForm = ({ API_HOST, setIsAuthenticated, setShowProfile, setShowRegisterForm }) => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
+    const [loading, setLoading] = useState(false); // Добавлено состояние loading
 
     const handleRegister = async (e) => {
         e.preventDefault();
+        setLoading(true); // Блокируем форму
         try {
             const response = await axios.post(`${API_HOST}/api/auth/register`, { username, email, password });
             sessionStorage.setItem('token', response.data.token);
             setIsAuthenticated(true);
             setShowProfile(true);
-            Toastify({ text: `Здравствуйте, ${username}! 
-                            Вы были успешно зарегистрированы.
-                          И отправили таблицу на Вашу эл.почту`, duration: 3000, gravity: 'top', position: 'right', style: { background: '#00cc00' } }).showToast();
+            Toastify({
+                text: `Здравствуйте, ${username}!
+                Вы успешно зарегистрированы. Таблица отправлена на Вашу эл.почту.`,
+                duration: 3000,
+                gravity: 'top',
+                position: 'right',
+                style: { background: '#00cc00' },
+            }).showToast();
         } catch (error) {
-            Toastify({ text: error.response?.data?.message || 'Ошибка регистрации', duration: 3000, gravity: 'top', position: 'right', style: { background: '#ff0000' } }).showToast();
+            Toastify({
+                text: error.response?.data?.message || 'Ошибка регистрации',
+                duration: 3000,
+                gravity: 'top',
+                position: 'right',
+                style: { background: '#ff0000' },
+            }).showToast();
+        } finally {
+            setLoading(false); // Разблокируем форму
         }
     };
 
@@ -30,23 +45,53 @@ const RegisterForm = ({API_HOST, setIsAuthenticated, setShowProfile, setShowRegi
             <h2>Регистрация</h2>
             <Form.Group controlId="username">
                 <Form.Label>Имя пользователя</Form.Label>
-                <Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
+                <Form.Control
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    disabled={loading} // Делаем поле неактивным
+                />
             </Form.Group>
             <Form.Group controlId="email">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                <Form.Control
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    disabled={loading} // Делаем поле неактивным
+                />
             </Form.Group>
             <Form.Group controlId="password">
                 <Form.Label>Пароль</Form.Label>
                 <InputGroup>
-                    <Form.Control type={showPassword ? 'text' : 'password'} value={password} onChange={(e) => setPassword(e.target.value)} required />
-                    <InputGroup.Text className="FaEye-button" onClick={() => setShowPassword(!showPassword)} >
+                    <Form.Control
+                        type={showPassword ? 'text' : 'password'}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        disabled={loading} // Делаем поле неактивным
+                    />
+                    <InputGroup.Text
+                        className="FaEye-button"
+                        onClick={() => setShowPassword(!showPassword)}
+                        style={{ cursor: 'pointer' }}
+                    >
                         {showPassword ? <FaEyeSlash /> : <FaEye />}
                     </InputGroup.Text>
                 </InputGroup>
             </Form.Group>
-            <Button type="submit">Зарегистрироваться</Button>
-            <Button variant="link" onClick={() => setShowRegisterForm(false)}>Авторизация</Button>
+            <Button type="submit" disabled={loading}>
+                {loading ? 'Загрузка...' : 'Зарегистрироваться'}
+            </Button>
+            <Button
+                variant="link"
+                onClick={() => setShowRegisterForm(false)}
+                disabled={loading} // Блокируем кнопку
+            >
+                Авторизация
+            </Button>
         </Form>
     );
 };
